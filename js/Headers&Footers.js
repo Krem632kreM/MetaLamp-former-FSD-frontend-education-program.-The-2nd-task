f36b3f9cdebd101087aa1f2fd35a528551ca02db
Krem632kreM <krempinatatyana@gmail.com>
2022-07-11 18:14:41 +0400

    create: header

---
+++ js/Headers&Footers.js
@@ -0,0 +1,18 @@
+const logInBtn = document.querySelector('#login')
+const signUpBtn = document.querySelector('#signup')
+const mainNav = document.querySelector('.header__container')
+console.log(mainNav)
+logInBtn.addEventListener('click', () => {
+  logInBtn.style.display = 'none'
+  signUpBtn.style.display = 'none'
+  const verticalLine = document.createElement('div');
+  verticalLine.style = `width: 1px; 
+  background-color: rgba(31, 32, 65, 0.1); 
+  height: 45px; 
+  float: left;`
+  mainNav.appendChild(verticalLine)
+
+  const userName = document.createElement('div');
+  userName.textContent = `User Name`
+  mainNav.appendChild(userName)
+})
--- js/UIKit.js
+++ js/UIKit.js
@@ -3,6 +3,8 @@ const radioButtonsBlockInputs = document.querySelectorAll('.radio_buttons > .rad
 const toggleInputs = document.querySelectorAll('.toggle > .inactive')
 const likeButton = document.querySelectorAll('.like_button > .inactive')
 const rateButtonBlock = document.querySelectorAll('.rate_button')
+const dropdown = document.querySelectorAll('.dropdown')
+dropdownRooms = document.querySelectorAll('.dropdown_rooms')
 
 /*RANGE*/
 const rangeContainerAll = document.querySelectorAll('.range__container')
@@ -69,6 +71,79 @@ rateButtonBlock.forEach((item) => {
 })
 })
 
+const dropdownButtonTitle = (number) => {
+  if (number === 11 || number === 12 || number === 13 || number === 14) return 'гостей'
+  else if (number % 10  === 1) return 'гость'
+  else if (number % 10  === 2 || number % 10  === 3 || number % 10  === 4 ) return 'гостя'
+  else return 'гостей'
+}
+
+dropdown.forEach(item => {
+  item.querySelector('.dropdown__button').addEventListener('click', () => {
+
+    if (item.querySelector('.dropdown__list').style.display === "flex") {
+      item.querySelector('.dropdown__list').style.display = "none"
+      item.querySelector('.dropdown__button').style.borderRadius = '4px'
+      item.querySelector('.dropdown__button').style.setProperty('--sq', 'rotate(0deg)')
+    } else {
+      item.querySelector('.dropdown__list').style.display = "flex"
+      item.querySelector('.dropdown__button').style.borderRadius = '4px 4px 0 0'
+      item.querySelector('.dropdown__button').style.setProperty('--sq', 'rotate(180deg)')
+    }
+  })
+    let countDropdownForClear = 0;
+  item.querySelectorAll('.dropdown__list > .dropdown__list-item > .count').forEach(elem => {
+    let countDropdown = 0;
+  
+    elem.querySelector('.count-next').addEventListener('click', () => {
+      countDropdown++
+      countDropdownForClear++
+      if (countDropdown>0) {
+        elem.querySelector('.count-prev').style.opacity = 1
+        item.querySelector('.dropdown__list > .buttons > .clear').style.display = 'block'
+        elem.querySelector('.count-prev').style.setProperty('pointer-events', 'all')
+      }
+      if (countDropdown<=0) {
+        elem.querySelector('.count-prev').style.opacity = 0.38
+        countDropdown = 0
+      }elem
+      elem.querySelector('.count-digit').textContent = countDropdown
+      item.querySelector('.dropdown__button').innerHTML = `${countDropdownForClear} ${dropdownButtonTitle(countDropdownForClear)}`
+    })
+
+    elem.querySelector('.count-prev').addEventListener('click', () => {
+      countDropdown--
+      countDropdownForClear--
+      if (countDropdownForClear <= 0) {
+        item.querySelector('.dropdown__list > .buttons > .clear').style.display = 'none';
+        item.querySelector('.dropdown__button').innerHTML = `Сколько гостей`
+        countDropdownForClear = 0;
+      }
+      if (countDropdown>0) {
+        elem.querySelector('.count-prev').style.opacity = 1
+        item.querySelector('.dropdown__list > .buttons > .clear').style.display = 'block'
+      }
+      if (countDropdown<=0) {
+        elem.querySelector('.count-prev').style.opacity = 0.38
+        elem.querySelector('.count-prev').style.setProperty('pointer-events', 'none')
+        countDropdown = 0
+      }
+
+      (countDropdownForClear === 0) ? item.querySelector('.dropdown__button').innerHTML = `Сколько гостей` : item.querySelector('.dropdown__button').innerHTML = `${countDropdownForClear} ${dropdownButtonTitle(countDropdownForClear)}`
+      elem.querySelector('.count-digit').textContent = countDropdown
+    })
+
+    item.querySelector('.dropdown__list > .buttons > .clear').addEventListener('click', () => {
+          countDropdownForClear = 0; 
+          countDropdown = 0;
+          elem.querySelector('.count-digit').textContent = countDropdown
+          item.querySelector('.dropdown__list > .buttons > .clear').style.display = 'none'
+          item.querySelector('.dropdown__button').innerHTML = `Сколько гостей`
+        })
+  })
+  
+})
+
 
 function insertSpace(number) {
   if (Math.ceil(number/1000)>1) {
@@ -98,5 +173,93 @@ rangeContainerAll.forEach((rangeContainer, index) => {
 
     rangePriceAll[index].textContent = insertSpace(inputLeftAll[index].value) + '₽ - ' +  insertSpace(inputRightAll[index].value) + '₽'
 
+  })
+})
+
+dropdownRooms.forEach(item => {
+  item.querySelector('.dropdown_rooms__button').addEventListener('click', () => {
+
+    if (item.querySelector('.dropdown_rooms__list').style.display === "flex") {
+      item.querySelector('.dropdown_rooms__list').style.display = "none"
+      item.querySelector('.dropdown_rooms__button').style.borderRadius = '4px'
+      item.querySelector('.dropdown_rooms__button').style.setProperty('--sq', 'rotate(0deg)')
+    } else {
+      item.querySelector('.dropdown_rooms__list').style.display = "flex"
+      item.querySelector('.dropdown_rooms__button').style.borderRadius = '4px 4px 0 0'
+      item.querySelector('.dropdown_rooms__button').style.setProperty('--sq', 'rotate(180deg)')
+    }
+  })
+    let countDropdownForClear = 0;
+  item.querySelectorAll('.dropdown_rooms__list > .dropdown_rooms__list-item > .count').forEach(elem => {
+    let countDropdown = 0;
+  
+    elem.querySelector('.count-next').addEventListener('click', (event) => {
+      countDropdown++
+      countDropdownForClear++
+      if (countDropdown>0) {
+        elem.querySelector('.count-prev').style.opacity = 1
+        elem.querySelector('.count-prev').style.setProperty('pointer-events', 'all')
+      }
+      if (countDropdown<=0) {
+        elem.querySelector('.count-prev').style.opacity = 0.38
+        countDropdown = 0
+      }
+      elem.querySelector('.count-digit').textContent = countDropdown
+      //item.querySelector('.dropdown_rooms__button').innerHTML = `${}`
+
+      const countDigits = event.target.closest('.dropdown_rooms__list').querySelectorAll('.dropdown_rooms__list-item > .count > .count-digit')
+
+      let str ='';
+
+      const Bedrooms = (number) => {
+        if (number === 11 || number === 12 || number === 13 || number === 14) return 'спален'
+        else if (number % 10  === 1) return 'спальня'
+        else if (number % 10  === 2 || number % 10  === 3 || number % 10  === 4 ) return 'спальни'
+        else return 'спален'
+      }
+
+      const Beds = (number) => {
+        if (number === 11 || number === 12 || number === 13 || number === 14) return 'кроватей'
+        else if (number % 10  === 1) return 'кровать'
+        else if (number % 10  === 2 || number % 10  === 3 || number % 10  === 4 ) return 'кровати'
+        else return 'кроватей'
+      }
+
+      const Bathrooms = (number) => {
+        if (number === 11 || number === 12 || number === 13 || number === 14) return 'ванных комнат'
+        else if (number % 10  === 1) return 'ванная комната'
+        else if (number % 10  === 2 || number % 10  === 3 || number % 10  === 4 ) return 'ванных комнаты'
+        else return 'ванных комнат'
+      }
+
+      countDigits.forEach((item,index) => { 
+        if(index===0) str += `${item.textContent} ${Bedrooms(item.textContent)}, `
+        if(index===1) str += `${item.textContent} ${Beds(item.textContent)}, `
+        if(index===2) str += `${item.textContent} ${Bathrooms(item.textContent)}`
+      
+      })
+      console.log(str)
+    })
+
+    elem.querySelector('.count-prev').addEventListener('click', () => {
+      countDropdown--
+      countDropdownForClear--
+      if (countDropdownForClear <= 0) {
+        item.querySelector('.dropdown_rooms__button').innerHTML = `Сколько гостей`
+        countDropdownForClear = 0;
+      }
+      if (countDropdown>0) {
+        elem.querySelector('.count-prev').style.opacity = 1
+      }
+      if (countDropdown<=0) {
+        elem.querySelector('.count-prev').style.opacity = 0.38
+        elem.querySelector('.count-prev').style.setProperty('pointer-events', 'none')
+        countDropdown = 0
+      }
+
+    /*(countDropdownForClear === 0) ? item.querySelector('.dropdown_rooms__button').innerHTML = `Количество комнат` : item.querySelector('.dropdown_rooms__button').innerHTML = `${dropdownRoomsButtonTitle( , , )}`*/
+
+      elem.querySelector('.count-digit').textContent = countDropdown
+    })
   })
 })
